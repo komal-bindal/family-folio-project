d92760174dc415ee3c23bbf40a5961feef510b74
Komal Bindal <bindalk19@gmail.com>
2024-11-25 09:18:19 +0530

    add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,174 @@
-import Image from 'next/image'
+import FamilyGroupPhoto from "../images/photo.jpeg";
+import RetroBg from "../images/oldPaper.jpg";
+import TreeLogo from "../images/tree3.png";
+import Leaves from "../images/Leaves1.png";
+import Logo from "../images/logo.png";
+import Bridge from "../images/bridge.png";
+import Arrow from "../images/arrow.png";
+import Book from "../images/book.png";
+import Image from "next/image";
+import Card from "../components/Card";
 
-export default function Home() {
+export const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="relative">
+      <div className="fixed top-0 z-20 bg-darkBrown h-1 w-full"></div>
+      <div className="absolute z-20 left-14 h-72 w-36">
+        <div className="bg-darkBrown h-40">
+          <Image className="h-[140%] w-[110$] p-[2%]" src={Logo} alt="Logo" />
+        </div>
+        <div className="bg-darkBrown h-32 border-t-white border-t-[1px] rounded-b-[50%]">
+          <div className="font-Ephesis text-white text-3xl w-full h-full flex flex-col items-center justify-center -rotate-[20deg]">
+            <div className="">Tracing</div>
+            <div className="">Heritage</div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="bg-lightBrown h-[660px] w-full relative  px-[6%] py-[100px]">
+        <div className="relative h-full w-full">
+          <div className="absolute bottom-[5%] w-[50%] h-[400px] flex items-end">
+            <div className="absolute top-0 bg-white w-full h-[100px] clip-border"></div>
+            <Image
+              className="h-[390px] w-full border"
+              src={FamilyGroupPhoto}
+              alt="FamilyGroupImage"
+            />
+          </div>
+          <div className="absolute top-0 right-[20%] w-[20%] h-[300px]">
+            <Image
+              className="h-full w-full rounded-t-[150px]"
+              src={FamilyGroupPhoto}
+              alt="FamilyGroupImage"
+            />
+          </div>
+          <div className="absolute bottom-0 left-[35%] w-[40%] h-[240px]">
+            <Image
+              className="h-full w-full clip-bg-border "
+              src={RetroBg}
+              alt="FamilyGroupImage"
+            />
+            <div className="absolute bottom-0 right-0 h-full rotate-[20deg] scale-125">
+              <Image
+                className="h-full w-full clip-bg-border "
+                src={Leaves}
+                alt="FamilyGroupImage"
+              />
+            </div>
+            <div className="absolute bottom-0 h-full p-[15%] font-DM_Serif_Text text-darkGreen text-3xl">
+              <span>Explore Your Ancestry, Connect Your Roots</span>
+              <button className="absolute bottom-0 left-[15%] bg-darkGreen text-white text-xl px-[5%] py-[3%] uppercase">
+                Start Journey
+              </button>
+            </div>
+          </div>
+          <div className="absolute right-0 bottom-0 w-[25%] h-[200px] bg-imageBg border-b-2 border-b-lightGreen flex justify-center items-center">
+            <Image
+              className="h-[90%] w-[90%]"
+              src={FamilyGroupPhoto}
+              alt="FamilyGroupImage"
+            />
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="bg-lightishBrown h-[120px] w-full relative flex justify-between px-[15%] py-[2%]">
+        <Card imageSrc={TreeLogo} title={"Discover Roots"} />
+        <span className="h-full w-1 border-r-[1px] border-r-lightBrownText" />
+        <Card imageSrc={Bridge} title={"Connect Generations"} />
+        <span className="h-full w-1 border-r-[1px] border-r-lightBrownText" />
+        <Card imageSrc={Book} title={"Preserve Legacy"} />
       </div>
-    </main>
-  )
-}
+      <div className="h-[500px] w-full relative flex ">
+        <div className="w-[60%] h-full bg-darkGreen text-white font-Lexend_Deca flex flex-col pl-[10%] items-left justify-center">
+          <div className="font-Ephesis text-4xl">Whatcha Here For?</div>
+          <p className="text-base pt-[5%] pb-[2%] font-thin">
+            You're here to{" "}
+            <span className="font-semibold">find your roots</span>,
+            <br />
+            And trace the paths you've never known.
+            <br />
+            To <span className="font-semibold">preserve your legacy</span>,
+            <br />
+            And keep your family's story grown.
+          </p>
+          <p className="text-base font-thin">
+            You're here to{" "}
+            <span className="font-semibold">build your tree</span>,
+            <br />
+            Branch by branch, a tale untold.
+            <br />
+            To{" "}
+            <span className="font-semibold">explore your past with pride</span>,
+            <br />
+            And share the legacy you'll unfold.
+          </p>
+        </div>
+        <div className="w-[40%] h-full bg-darkBrown">
+          <div className="absolute left-[40%] top-[10%] w-[30%] h-[250px]">
+            <Image
+              className="h-full w-full"
+              src={FamilyGroupPhoto}
+              alt="FamilyGroupImage"
+            />
+          </div>
+          <div className="absolute top-[5%] left-[72%] w-[15%] h-[250px]">
+            <Image
+              className="h-full w-full rounded-t-[150px]"
+              src={FamilyGroupPhoto}
+              alt="FamilyGroupImage"
+            />
+          </div>
+          <div className="absolute left-[55%] bottom-[15%] w-[28%] h-[200px] bg-imageBg flex justify-center items-center origin-bottom -rotate-6">
+            <Image
+              className="h-[90%] w-[90%]"
+              src={FamilyGroupPhoto}
+              alt="FamilyGroupImage"
+            />
+          </div>
+          <div className="absolute bottom-0 right-[2%] w-[30%] h-[180px] ">
+            <Image
+              className="h-full w-full clip-border-bottom"
+              src={RetroBg}
+              alt="FamilyGroupImage"
+            />
+            <div className="absolute bottom-0 h-full p-[10%] font-DM_Serif_Text text-darkGreen text-2xl">
+              <span>Let Us</span>
+              <p className="text-base">
+                Help you in creating a family tree that connects your past to
+                your future.
+              </p>
+            </div>
+          </div>
+          <div className="absolute bottom-[15%] right-[33%] w-[20%] h-[20%]">
+            <Image
+              className="h-full w-full clip-border-bottom"
+              src={Arrow}
+              alt="FamilyGroupImage"
+            />
+          </div>
+          <div className="absolute bottom-[22%] right-[50%] font-Ephesis text-4xl text-white lowercase">
+            With Love
+          </div>
+        </div>
+      </div>
+      <div className="relative h-[500px] w-full relative flex bg-grey">
+        <div className="absolute left-[10%] top-[40%] w-[20%] h-[180px] bg-imageBg border-b-2 border-b-lightGreen flex justify-center items-center z-10">
+          <Image
+            className="h-[90%] w-[90%]"
+            src={FamilyGroupPhoto}
+            alt="FamilyGroupImage"
+          />
+        </div>
+        <div className="absolute top-[10%] left-[25%] w-[20%] h-[400px]">
+          <Image
+            className="h-full w-full rounded-t-[150px]"
+            src={FamilyGroupPhoto}
+            alt="FamilyGroupImage"
+          />
+        </div>
+      </div>
+      <div className="h-[300px] w-full relative flex bg-lightestBrown"></div>
+    </div>
+  );
+};
+
+export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,54 @@
-import type { Config } from 'tailwindcss'
+import { DM_Serif_Text, Lexend_Deca } from "next/font/google";
+import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin"; // Import the plugin type helper
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        white: "#FFFFFF",
+        lightBrownText: "#f9e4d1",
+        darkBrown: "#8D4523",
+        lightBrown: "#ECDED1",
+        primary: "#1DA1F2",
+        secondary: "#14171A",
+        accent: "#FFAD1F",
+        imageBg: "#E5D6C6",
+        lightGreen: "#91877A",
+        darkGreen: "#103A39",
+        lightishBrown: "#C6A386",
+        lightestBrown: "#EEDDCD",
+        grey: "#EEE9E3",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        Ephesis: ["Ephesis"],
+        DM_Serif_Text: ["DM Serif Text"],
+        Lexend_Deca: ["Lexend Deca"],
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".clip-torn": {
+          clipPath: `polygon(
+            0% 100%, 10% 90%, 20% 100%, 30% 90%, 40% 100%, 
+            50% 90%, 60% 100%, 70% 90%, 80% 100%, 90% 90%, 100% 100%
+          )`,
+        },
+      });
+    }),
+  ],
+};
+
+export default config;
